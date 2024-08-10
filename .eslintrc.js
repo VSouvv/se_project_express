0a14a707a27e0967af88c0e784e381627cc348a5
VSouvv <161387076+VSouvv@users.noreply.github.com>
2024-08-10 10:20:04 -0500

    set up a linter complete

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+      es2021: true,
+      node: true,
+    },
+    extends: ["eslint:recommended", "airbnb-base", "prettier"],
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: [".eslintrc.{js,cjs}"],
+        parserOptions: {
+          sourceType: "script",
+        },
+      },
+    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+        "no-console": ["warn", { allow: ["error"] } ],
+        "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    },
+  };
