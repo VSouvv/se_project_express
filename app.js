828a7ee102351eaa094e54ada0cfa58f9e34e126
VSouvv <161387076+VSouvv@users.noreply.github.com>
2024-08-10 11:14:53 -0500

    create Mongoose connection

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((e) => console.error(e));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
