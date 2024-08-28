75c541020b919b1e0adff10871a283c92db014ba
VSouvv <161387076+VSouvv@users.noreply.github.com>
2024-08-28 08:59:20 -0500

    add error text to every message constant

--- utils/errors.js
+++ utils/errors.js
@@ -8,12 +8,18 @@ const ERROR_CODES = {
 };
 
 const ERROR_MESSAGES = {
-  BAD_REQUEST,
-  AUTHORIZATION_ERROR,
-  FORBIDDEN,
-  NOT_FOUND,
-  DUPLICATE_EMAIL_EXISTS,
-  SERVER_ERROR,
+  BAD_REQUEST:
+    " invalid data passed to the methods for creating an item/user or updating an item",
+  AUTHORIZATION_ERROR:
+    " there is something wrong with authorization; i.e., an incorrect email or password, the token is invalid, or an unauthorized user is trying to access protected routes.",
+  FORBIDDEN:
+    "This is a permissions issue. The user is trying to remove the card of another user.",
+  NOT_FOUND:
+    "There is no user or clothing item with the requested id or the request was sent to a non-existent address.",
+  DUPLICATE_EMAIL_EXISTS:
+    "When registering, the user entered an email address that already exists on the server.",
+  SERVER_ERROR:
+    "Default error. Accompanied by the message: “An error has occurred on the server.",
 };
 
 module.exports = {
