10c853013a074756cf23fbcc2ccb582371ccdd8a
VSouvv <161387076+VSouvv@users.noreply.github.com>
2024-11-14 19:13:25 -0600

    remove return to fix linter error

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
   const { statusCode = 500, message = "An error occurred on the server" } = err;
-  return res.status(statusCode).send({
+  res.status(statusCode).send({
     message,
   });
   next();
